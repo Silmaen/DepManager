6f41b9a6a2446aa22f9bf7be65a7e45ae2d9d335
Silmaen <genteur.slayer@laposte.net>
2023-04-07 10:05:38 +0200

    Better linux integration

--- src/depmanager/api/internal/dependency.py
+++ src/depmanager/api/internal/dependency.py
@@ -262,7 +262,7 @@ class Dependency:
                 self.base_path = None
                 return
             self.read_edp_file()
-            search = list(self.base_path.rglob("*config.cmake"))
+            search = list(self.base_path.rglob("*onfig.cmake"))
             if len(search) > 0:
                 self.cmake_config_path = ";".join([str(s.parent) for s in search])
         elif type(data) in [str, dict]:
