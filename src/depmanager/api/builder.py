7ce968ecbc8f2790259a211190687cf119c34a1f
Silmaen <genteur.slayer@laposte.net>
2024-01-14 23:57:12 +0100

    Allow recursive search for recipes.

--- src/depmanager/api/builder.py
+++ src/depmanager/api/builder.py
@@ -11,6 +11,66 @@ from depmanager.api.local import LocalManager
 from depmanager.api.recipe import Recipe
 
 
+def find_recipes(location: Path, depth: int = -1):
+    """
+    List all recipes in the given location.
+    :param location: Starting location.
+    :param depth: Folder's depth of search, negative means infinite.
+    :return: List of recipes
+    """
+    from importlib.util import spec_from_file_location, module_from_spec
+    from inspect import getmembers, isclass
+
+    recipes = []
+
+    all_py = []
+
+    def search_rep(rep: Path, dep: int):
+        """
+        Recursive search function, a bit faster than a rglob.
+        :param rep: Folder to look.
+        :param dep: Current depth of search
+        """
+        for entry in rep.iterdir():
+            if entry.is_file():
+                if entry.suffix != ".py":
+                    continue
+                if "conan" in entry.name:  # skip conan files
+                    continue
+                if "doxy" in entry.name:  # skip doxygen files
+                    continue
+                with open(entry, "r") as f:
+                    if f.readline().startswith("#!"):  # skip files with a shebang
+                        continue
+                all_py.append(entry.resolve())
+            elif entry.is_dir() and (depth < 0 or dep < depth):
+                search_rep(entry, dep + 1)
+
+    search_rep(location, 0)
+    print(f"found {len(all_py)} python files")
+    idx = 0
+    for file in all_py:
+        try:
+            spec = spec_from_file_location(file.name, file)
+            mod = module_from_spec(spec)
+            spec.loader.exec_module(mod)
+            file_has_recipe = False
+            for name, obj in getmembers(mod):
+                if isclass(obj) and name != "Recipe" and issubclass(obj, Recipe):
+                    recipes.append(obj())
+                    file_has_recipe = True
+            if file_has_recipe:
+                idx += 1
+        except Exception as err:
+            print(
+                f"Exception during analysis of file {file}, module {name} : {err}",
+                file=stderr,
+            )
+            continue
+    print(f"found {len(recipes)} recipes in {idx} files")
+    return recipes
+
+
 class Builder:
     """
     Manager for building packages.
@@ -20,13 +80,12 @@ class Builder:
         self,
         source: Path,
         temp: Path = None,
+        depth: int = 0,
         local: LocalSystem = None,
         cross_info=None,
     ):
         if cross_info is None:
             cross_info = {}
-        from importlib.util import spec_from_file_location, module_from_spec
-        from inspect import getmembers, isclass
 
         self.cross_info = cross_info
         self.generator = ""
@@ -41,18 +100,9 @@ class Builder:
             self.temp = self.local.temp_path / "builder"
         else:
             self.temp = temp
-        self.recipes = []
-        for file in self.source_path.iterdir():
-            if not file.is_file():
-                continue
-            if file.suffix != ".py":
-                continue
-            spec = spec_from_file_location(file.name, file)
-            mod = module_from_spec(spec)
-            spec.loader.exec_module(mod)
-            for name, obj in getmembers(mod):
-                if isclass(obj) and name != "Recipe" and issubclass(obj, Recipe):
-                    self.recipes.append(obj())
+        if self.local.verbosity > 0:
+            print(f"Recipes search ..")
+        self.recipes = find_recipes(self.source_path, depth)
 
     def has_recipes(self):
         """
